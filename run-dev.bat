46596e1abb8d9fa0b701a23116fe10458204359d
Daniele Santini <danysan95@gmail.com>
2023-02-27 17:26:33 +0100

    Fix configuration

---
+++ run-dev.bat
@@ -0,0 +1,3 @@
+cd osm-wikidata-map-framework-burial
+docker-compose --profile dev up -d
+npm run dev
---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd $(dirname "$0")
+/usr/bin/git fetch --recurse-submodules
+/usr/bin/git pull --recurse-submodules
+
+# https://docs.docker.com/compose/profiles/#enable-profiles
+cd osm-wikidata-map-framework-burial
+export COMPOSE_PROFILES=prod,promtail
+/usr/local/bin/docker-compose pull
+if [ '--build' == "$1" ]; then
+    /usr/local/bin/docker-compose build
+    /usr/local/bin/docker-compose push
+fi
+/usr/local/bin/docker-compose up --detach --always-recreate-deps
+/usr/bin/docker image prune -af
+watch /usr/bin/docker ps
